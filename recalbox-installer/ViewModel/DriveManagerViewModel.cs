b6e8013c460061e4a1beb3c7f8912bdbeb75ee7c
MikaXII <mikael.joubert@supinfo.com>
2015-09-10 17:20:12 +0200

    Update Model And View Model

---
+++ recalbox-installer/ViewModel/DriveManagerViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using recalbox_installer.Annotations;
+using recalbox_installer.Model;
+
+namespace recalbox_installer.ViewModel
+{
+    class DriveManagerViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<string> _observableCollectionDriveLetter;
+
+        public DriveManagerViewModel()
+        {
+            // _observableCollectionDriveLetter = new ObservableCollection<string>(DriveManager.GetAllDrive());
+            Thread threadUpdate = new Thread(this.UpdateDriveAvailable);
+            threadUpdate.Start();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<string> DriveLetter
+        {
+            get { return _observableCollectionDriveLetter; }
+            set
+            {
+                _observableCollectionDriveLetter = value;
+                OnPropertyChanged("DriveLetter");
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void UpdateDriveAvailable()
+        {
+            List<string> listOfAvailableDrive = new List<string>();
+    
+            while (true)
+            {
+                if (listOfAvailableDrive.Count != DriveManager.GetAllDrive().Count)
+                {
+                    listOfAvailableDrive = DriveManager.GetAllDrive();
+                    DriveLetter = new ObservableCollection<string>(listOfAvailableDrive);   
+                }
+                Thread.Sleep(100);
+            }
+
+        }
+    }
+}
---
+++ recalbox-installer/ViewModel/RecalboxReleaseViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using recalbox_installer.Annotations;
+using recalbox_installer.Model;
+
+namespace recalbox_installer.ViewModel
+{
+    class RecalboxReleaseViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<RecalboxRelease> _observableCollectionRecalbox; 
+
+        public RecalboxReleaseViewModel()
+        {
+            getReleaseFromGithub(@"https://api.github.com/repos/digitalLumberjack/recalbox-os/releases");
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void getReleaseFromGithub(string githubLink)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers[HttpRequestHeader.Accept] = "application/json";
+                client.Headers[HttpRequestHeader.ContentType] = "application/json";
+                var json = client.DownloadString(githubLink);
+
+            }
+
+        }
+    }
+}
